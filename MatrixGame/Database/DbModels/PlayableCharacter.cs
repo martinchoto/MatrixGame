d956b1fa2eed67c35ae8a514255882d4fba2040c
martinchoto <martin_stalev03@abv.bg>
2025-06-09 02:00:37 +0300

    added db and made logic

---
+++ MatrixGame/Database/DbModels/PlayableCharacter.cs
@@ -0,0 +1,23 @@
+﻿using MatrixGame.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixGame.Database.DbModels
+{
+	public class PlayableCharacter : Character
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Class { get; set; } = null!;
+
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		[NotMapped]
+		public override char Symbol => ' ';
+	}
+}
